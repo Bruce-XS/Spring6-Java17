0ccb48ed64fe957956cdb351a0854e4faca6ea9c
bruce-xiao <johnfxiao@163.com>
2023-03-16 17:07:38 +0800

    spring6 jdk17 lean code test by bruce xiao

---
+++ test/src/main/java/cn/spring6/Main.java
@@ -0,0 +1,18 @@
+package cn.spring6;
+
+/**
+ * GFKDXS
+ *
+ * @version 1.0
+ * @Description:
+ * @ClassName: Main
+ * @author: Bruce Xiao
+ * @date: 2023-03-17
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("hello spring6");
+    }
+
+}
